d66eba83f6745c5cf5a63839f0ec2a6cd8760f78
Kartik Chhipa <97423151+kartikchhipa@users.noreply.github.com>
2023-01-31 00:16:38 +0530

    commit

--- backend_task/settings.py
+++ backend_task/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY',env('SECRET_KEY')) #
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG',env('DEBUG'))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
